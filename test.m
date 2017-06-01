bb5aaad5bd5528288f9d708c27f0e472e1e8f74a
Jesse Frey <jesse.frey@nist.gov>
2017-06-01 13:58:30 -0600

    added buffer over and under run checks

--- test.m
+++ test.m
@@ -77,6 +77,20 @@ for k=1:N
 
 end
 
+%check for buffer over runs
+if(any(overRun))
+    fprintf('There were %i buffer over runs\n',sum(overRun));
+else
+    fprintf('There were no buffer over runs\n');
+end
+
+%check for buffer over runs
+if(any(underRun))
+    fprintf('There were %i buffer under runs\n',sum(underRun));
+else
+    fprintf('There were no buffer under runs\n');
+end
+
 %new figure
 figure;
 
