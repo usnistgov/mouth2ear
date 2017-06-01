56dcf24eeec31f82464ba8b02c2e7c423b87e7c9
Jesse Frey <jesse.frey@nist.gov>
2017-06-01 15:17:42 -0600

    Changed mkchirp to start and end at zero
    This is usefull to eliminate sharp transitions in the waveform for
    waveform detection

--- mkchirp.m
+++ mkchirp.m
@@ -5,8 +5,11 @@ chan=1;
 t = 0:1/fs:c_len;
 fo = 200;
 f1 = 800;
-y0 = 0.01*chirp(t,fo,c_len,f1,'logarithmic');
+y0 = 0.01*chirp(t,fo,c_len,f1,'logarithmic',90);
 
-y=repmat(y0,chan,rep);
+%find last zero in the waveform
+idx=find(abs(y0)<1e-3,1,'last');
+
+y=repmat(y0(1:idx),chan,rep);
 
 save('chirp.mat','y','fs');
