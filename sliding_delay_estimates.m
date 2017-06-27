34cb10fec292221f322a326206831e7f33fc692a
Jesse Frey <jesse.frey@NIST.GOV>
2017-06-27 13:10:31 -0600

    Refactor comments

--- sliding_delay_estimates.m
+++ sliding_delay_estimates.m
@@ -1,42 +1,44 @@
-function Delays=sliding_delay_estimates(infile,varargin)
-
-%This function performs a sequence of windowed time delay estimates
-%between two channels of a stereo
-%.wav file. Results are plotted as figure 1, and are also returned to the
-%calling code in the column vector variable named 'Delays.'
+function Delays=sliding_delay_estimates(test,ref,fs,varargin)
+% SLIDING_DELAY_ESTIMATES perform sequence of windowed time delay estimates
 %
-%Written by Stephen Voran at the Institute for Telecommunication Sciences,
-%325 Broadway, Boulder, Colorado, USA, svoran@its.bldrdoc.gov
-%March 30, 2016
+%   SLIDING_DELAY_ESTIMATES(test,ref,fs) performs delay estimates between
+%       ref (input to the system under test) and test (output from system
+%       under test)
 %
-%Use:  Delays=sliding_delay_estimates(infile,winLength,winStep)
+%   SLIDING_DELAY_ESTIMATES(test,ref,fs,winLength) specifies the window
+%       length in seconds, winLength, instead of using the default of 4
+%       seconds. The window length is the length of time to use for the
+%       delay estimates
 %
-%infile is a string naming the desired stereo .wav file (including path if not local).
-%File must use sample rate 8, 16, 24, 32, or 48k.
-%File should have SUT input on Left Channel and SUT output on Right Channel
+%   SLIDING_DELAY_ESTIMATES(test,ref,fs,winLength,winStep) specifies the
+%       window step in seconds, winStep, instead of using the default of 2
+%       seconds. The window step is the amount of time the window moves
+%       forward for each subsequent time delay estimate.
 %
-%winLength is an optional window length value, defaults to 4 seconds if not
-%specified. This means each time delay estimate will use 4 seconds of
-%signal.
+%   Delays=SLIDING_DELAY_ESTIMATES(__) returns the delays as a vector
+%   instead of plotting them. Delays holds one delay estimate in ms for
+%   each time window. The resolution of these estimates is 1/8kHz. The
+%   accuracy of these estimates depends of the level of background noise
+%   and speech distortion in the input waveform
 %
-%winStep is optional window step value, defaults to 2 second if not
-%specified.  This means the windows move forward 2 seconds for each
-%subsequent time delay estimate.
+% Notes:  requires access to ITS_delay_est.m, written at ITS
+%   requires access to resample function included in Matlab Signal Processing Toolbox
 %
-%delays holds one delay estimate in ms for each time window,
-%resolution of these estimates is 0.125 ms (i.e. 1 smp at fs=8000)
-%accuraccy of these estimates will depend on the level of background noise 
-%and speech distortion in the .wav file
-%
-%Notes:  requires access to ITS_delay_est.m, written at ITS
-%        requires access to resample function included in Matlab Signal Processing Toolbox
+% Written by Stephen Voran at the Institute for Telecommunication Sciences,
+% 325 Broadway, Boulder, Colorado, USA, svoran@its.bldrdoc.gov
+% March 30, 2016
+% Modification By Jesse Frey June, 2017
 
 
 %create new input parser
 p=inputParser();
 
-%add filename argument
-addRequired(p,'infile',@ischar);
+%add test signal parameter
+addRequired(p,'test',@(l)validateattributes(l,{'numeric'},{'real','finite','vector'}));
+%add reference signal parameter
+addRequired(p,'ref',@(l)validateattributes(l,{'numeric'},{'real','finite','vector'}));
+%add sample rate parameter
+addRequired(p,'fs',@(l)validateattributes(l,{'numeric'},{'scalar','finite'}));
 
 %add window length argument
 addOptional(p,'winLength',4,@(l)validateattributes(l,{'numeric'},{'scalar','positive'}));
@@ -47,18 +49,19 @@ addOptional(p,'winStep',2,@(l)validateattributes(l,{'numeric'},{'scalar','positi
 p.CaseSensitive= true;
 
 %parse inputs
-parse(p,infile,varargin{:});
+parse(p,test,ref,fs,varargin{:});
 
-%-----read .wav file and test sample rate-----
-[x,fs]=audioread(p.Results.infile);
+%check sample rate
 if ~any(fs==[48000 32000 24000 16000 8000])
-    error('Audio file must use sample rate 8, 16, 24, 32, or 48k')
+    error('Audio must use sample rate 8, 16, 24, 32, or 48k')
 end
 
-
-N=size(x,1); %number of samples available
-Nwin=round(p.Results.winLength*fs); %number of samples needed for each window
-Nstep=round(p.Results.winStep*fs);  %number of sampels to advance between windows
+%number of samples available in both files
+N=min(length(p.Results.test),length(p.Results.ref));
+%number of samples needed for each window
+Nwin=round(p.Results.winLength*fs);
+%number of sampels to advance between windows
+Nstep=round(p.Results.winStep*fs);
 
 firstSmp=1;
 lastSmp=firstSmp+Nwin-1;
@@ -68,8 +71,10 @@ Times=[];  %Will hold time of center of window
 
 %-----Loop to perform all possible time delay estimates-----
 while lastSmp<=N
-    ref=x(firstSmp:lastSmp,1); %extract left channel signal (SUT input)
-    test=x(firstSmp:lastSmp,2); %extract right channel signal (SUT ouput)
+    %get a section of the SUT input signal
+    ref=p.Results.ref(firstSmp:lastSmp); 
+    %get a section of the SUT output signal
+    test=p.Results.test(firstSmp:lastSmp); 
 
     %-----Apply our delay estimation tool to extracted portions of signal-----    
     temp=ITS_delay_est(resample(ref,1,fs/8000),resample(test,1,fs/8000),'f');
@@ -83,8 +88,10 @@ while lastSmp<=N
     lastSmp=lastSmp+Nstep;    
 end
 
-Delays=Delays/8; %convert from samples to ms
-Times=Times/fs; %convert from samples to seconds
+%convert from samples to ms
+Delays=Delays/8;
+%convert from samples to seconds
+Times=Times/fs; 
 
 %check if output arguments were given
 if(nargout==0)
