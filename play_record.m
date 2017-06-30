3b38f9df709ea79898a1193c65fbebd94cf01eaa
Jesse Frey <jesse.frey@NIST.GOV>
2017-06-30 14:48:51 -0600

    Refactored the loop in play_record
    Changed the loop to count buffers sent instead of sample indicies also
    fixed extra zeros at the end of recorded data

--- play_record.m
+++ play_record.m
@@ -6,21 +6,24 @@ function [y,underRun,overRun]=play_record(apr,x)
     %get buffer size
     bsz=apr.BufferSize;
 
+    %calculate the number of loops needed
+    runs=ceil(length(x)/bsz+5);
+    
     %initialize recive audio buffer
-    y=zeros(size(x)+[5*bsz 0]);
+    y=zeros((runs-1)*bsz,size(x,2));
     
     %zerro under and over runs
     underRun=0;
     overRun=0;
     
-    for k=bsz:bsz:(length(y))
+    for k=1:runs
         
-        if(k<=length(x))
+        if(k*bsz<=length(x))
             %get a chunk of data
-            datin=x((k-bsz+1):k);
-        elseif((k-bsz+1)<=length(x))
+            datin=x((bsz*(k-1)+1):(bsz*k));
+        elseif(k*bsz>=length(x))
             %get data to end of file
-            datin=x((k-bsz+1):end);
+            datin=x((bsz*(k-1)+1):end);
             %add zeros to buffer size
             datin(end+1:bsz)=zeros(bsz-length(datin),1);
         else
@@ -32,9 +35,9 @@ function [y,underRun,overRun]=play_record(apr,x)
         [datout,ur,or]=apr(datin);
 
         %check if this is not the first run
-        if(k~=bsz)
+        if(k>1)
             %add data to array
-            y((k-2*bsz+1):(k-bsz))=datout;
+            y((bsz*(k-2)+1):(bsz*(k-1)))=datout;
         end
 
         %add under and over runs
