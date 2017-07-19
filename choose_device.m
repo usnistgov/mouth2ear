f43187e2db422478dc4c8c43ee91c8593e9aa9a5
Jesse Frey <jesse.frey@NIST.GOV>
2017-07-19 08:47:30 -0600

    Cleanup choose_device.m
    Remove abandoned device names and remove code to chose between core
    audio and bit accurate (Mac only).

--- choose_device.m
+++ choose_device.m
@@ -1,7 +1,7 @@
 function name=choose_device(apr)
 
     %list of sound device names to use
-    device_names={'UH-7000','M-Track','Focusrite','UMC','Scarlett'};
+    device_names={'UMC'};
 
     %get a list of the audio devices
     ad=apr.getAudioDevices();
@@ -11,26 +11,11 @@ function name=choose_device(apr)
 
     %check if there were more than one matching device
     if(length(idx)>1)
-        %type of driver to use
-        %dtype='(Bit Accurate)';
-        dtype='(Core Audio)';
         %print out devices found
         fprintf('Multiple devices found :\n');
         fprintf('\t%s\n',ad{idx});
-        %get the matching one
-        devIdxIdx=find(contains(ad(idx),dtype),1);
-        %make sure one met the criteria
-        if(~isempty(devIdxIdx))
-            %set the new index
-            idx=idx(devIdxIdx);
-            %print out which is being used
-            fprintf('Using "%s"\n',ad{idx});
-        else
-            %use the last
-            idx=idx(end);
-            %print which is used
-            fprintf('Could not find one matching "%s" using "%s" instead\n',dtype,ad{idx});
-        end
+        %use the last
+        idx=idx(end);
     end
     
     %return name
