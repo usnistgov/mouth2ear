3ca959b6f67d27190ecfca135c58cdc7830d35db
Jesse Frey <jesse.frey@NIST.GOV>
2017-08-11 12:58:10 -0600

    Initial commit for radioInterface class
    The radio interface class is used to communicate with the radios. This
    will be done using a microcontroller attached to the push to talk
    button. The radioInterface class is used for interacting with the
    microcontroler over a serial interface

---
+++ radioInterface.m
@@ -0,0 +1,52 @@
+classdef radioInterface < handle
+    %RADIOINTERFACE class to interface to raido Push To Talk button
+    
+    properties (Access = private)
+        sobj
+    end
+    
+    methods
+        %constructor, must be passed a serial port name
+        function obj = radioInterface(port)
+% RADIOINTERFACE creat a radio interface object
+%
+% obj = RADIOINTERFACE('port')
+%   Create a RADIOINTERFACE object using the specified serial port
+            
+            
+            %get serial port
+            obj.sobj=serial(port);
+            
+            %open serial port
+            fopen(obj.sobj);
+        end
+        %function to key or un-key the radio
+        function ptt(obj,state)      
+% PTT change the push to talk status of the radio interface
+%
+% ptt(state) if state is true then the PTT is set to transmit. if state is
+% false then the radio is set to not transmit
+            
+            
+            %check what the state is 
+            if(state)
+                fprintf(obj.sobj,'%s\n','ptt on');
+            else
+                fprintf(obj.sobj,'%s\n','ptt off');
+            end
+        end
+        
+        %delete method
+        function delete(obj)
+            %check if serial port is open
+            if(isvalid(obj.sobj))
+                %close serial port
+                fclose(obj.sobj);
+                %delete serial object
+                delete(obj.sobj);
+            end
+        end
+    end
+    
+end
+
