f8e935e1bcf9cd78541fbe36b49ff28ebb2cf78f
pscrmcv <>
2017-09-15 12:11:42 -0600

    Rerun on, different files to process

--- process_sessions.m
+++ process_sessions.m
@@ -1,17 +1,19 @@
 close all
 clear all
+
 tx_path = 'C:\MCV\device-tst\tx-data';
 proc_path = 'C:\MCV\device-tst\proc-data';
 
 % descr = 'US36-pullout-VHF-trunked';
-% descr = 'US36-pullout-UHF-trunked';
+descr = 'US36-pullout-UHF-trunked';
 % descr = 'US36-pullout-UHF-direct';
 % descr = 'two-loc-2tc';
-descr = '2loc-2tc-characterization';
+% descr = '2loc-2tc-characterization';
+% descr = 'VHF-direct-NCAR';
 
 datName = [proc_path '\' descr, '.mat'];
 
-rerun = 0;
+rerun = 1;
 if(~exist(datName)||rerun)
     disp('Extracting data...')
     data = loadData(tx_path, 'descriptor', descr, 'datType', '2loc', 'saveDir', 'proc-data');
@@ -27,6 +29,7 @@ if(~exist(figDir))
 end
 
 fullData = cell2mat(data(:,2)');
+
 [nWindows,~] = size(fullData);
 timeData = fullData(:);
 tit = strrep(descr, '-', ' ');
