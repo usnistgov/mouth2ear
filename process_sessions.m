8b792296701f4a1bb42e8a525aa4df1e8a55b72e
pscrmcv <>
2017-09-18 14:33:59 -0600

    Processed 2loc-2tc-lab-VHF-Trunked

--- process_sessions.m
+++ process_sessions.m
@@ -5,15 +5,16 @@ tx_path = 'C:\MCV\device-tst\tx-data';
 proc_path = 'C:\MCV\device-tst\proc-data';
 
 % descr = 'US36-pullout-VHF-trunked';
-descr = 'US36-pullout-UHF-trunked';
+% descr = 'US36-pullout-UHF-trunked';
 % descr = 'US36-pullout-UHF-direct';
 % descr = 'two-loc-2tc';
 % descr = '2loc-2tc-characterization';
 % descr = 'VHF-direct-NCAR';
+descr = '2loc-2tc-lab-VHF-Trunked';
 
 datName = [proc_path '\' descr, '.mat'];
 
-rerun = 1;
+rerun = 0;
 if(~exist(datName)||rerun)
     disp('Extracting data...')
     data = loadData(tx_path, 'descriptor', descr, 'datType', '2loc', 'saveDir', 'proc-data');
@@ -23,7 +24,7 @@ end
 
 [nFiles,~] = size(data);
 
-figDir = [pwd, '\figures\trials'];
+figDir = [pwd, '\figures'];
 if(~exist(figDir))
    mkdir(figDir); 
 end
@@ -33,27 +34,32 @@ fullData = cell2mat(data(:,2)');
 [nWindows,~] = size(fullData);
 timeData = fullData(:);
 tit = strrep(descr, '-', ' ');
+
 dataPlot = figure;
+dataPlot.InnerPosition(3) = 1120;
+subplot(1,2,1)
 xV = (1:length(timeData))/nWindows;
 plot(xV,timeData)
 xlabel('Trial Number')
 ylabel('Delay (s)')
 title([tit ' Data Sessions'])
 
-saveas(dataPlot, [figDir, '\', descr, '.fig'])
-saveas(dataPlot, [figDir, '\', descr, '.png'])
+% saveas(dataPlot, [figDir, '\', descr, '.fig'])
+% saveas(dataPlot, [figDir, '\', descr, '.png'])
 
-figDir = [pwd, '\figures\hists'];
-if(~exist(figDir))
-   mkdir(figDir); 
-end
+% figDir = [pwd, '\figures'];
+% if(~exist(figDir))
+%    mkdir(figDir); 
+% end
 
-dataPlot = figure;
+% dataPlot = figure;
+subplot(1,2,2)
 h = histogram(timeData);
-h.BinWidth = 1/4000;
+% h.BinWidth = 1/20000;
 mV = mean(timeData);
 sV = std(timeData);
 title([tit ' - Mean: ' num2str(mV) ' (s). STD: ' num2str(sV)])
 
 saveas(dataPlot, [figDir, '\', descr, '.fig'])
 saveas(dataPlot, [figDir, '\', descr, '.png'])
+saveas(dataPlot, [figDir,'\', descr, 'eps'], 'epsc')
