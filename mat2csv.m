1ce9242e526b0e8ff271807e70c34c223914911b
Jaden <jaden.pieper@nist.gov>
2020-10-08 16:02:42 -0600

    Added simple function to get csv values from mat file

---
+++ mat2csv.m
@@ -0,0 +1,18 @@
+function mat2csv(fname,outname)
+% Simple function to extract data from mouth2ear data in fname to a csv
+
+dat = load(fname);
+
+dly_vals = cellfun(@mean,dat.dly_its);
+
+try
+    fid= fopen(outname,'w');
+    fprintf(fid,'Mean Delay Per Trial (ms)\n');
+    fprintf(fid,'%d\n',dly_vals);
+    fclose(fid);
+catch ME
+    fclose(fid);
+    rethrow(ME);
+end
+
+end
